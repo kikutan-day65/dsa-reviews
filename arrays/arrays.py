89074755674d8c447df6f1e701fe4ce9f323f2ae
Haruto Mori <baystarsman@outlook.jp>
2023-05-03 13:17:22 +0900

    build a function to search an element in the array

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -2,6 +2,11 @@ def  access(list, index):
     elem = list[index]
     return elem
 
+def search(list, target):
+    if target in list:
+        return True
+    return False
+
 int_list = [1,2,3,4]
 char_list = ['a', 'b', 'c', 'd']
 
@@ -9,4 +14,10 @@ result = access(int_list, 3)
 print(result)
 
 result = access(char_list, 0)
+print(result)
+
+result = search(int_list, 2)
+print(result)
+
+result = search(char_list, 'x')
 print(result)
