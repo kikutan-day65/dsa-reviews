cd78ba578099f9a8ab829fb61e9d6bf333927888
Haruto Mori <baystarsman@outlook.jp>
2023-05-03 15:50:16 +0900

    build a function to insert an element at the given index in the array

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -7,6 +7,18 @@ def search(list, target):
         return True
     return False
 
+def insert(list, index, value):
+    if index == len(list):
+        list.append(value)
+        return True
+    
+    list.append(None)
+    for i in range(len(list) - 1, index, -1):
+        list[i] = list[i - 1]
+    
+    list[index] = value
+    return True
+
 my_list = [1,2,3,4]
 
 result = access(my_list, 3)
@@ -14,3 +26,9 @@ print(result)
 
 result = search(my_list, 2)
 print(result)
+
+insert(my_list, 4, 10)
+print(my_list)
+
+insert(my_list, 2, 10)
+print(my_list)
