b3deffab5a24293316a1eea6423f11cf53cd4857
Haruto Mori <baystarsman@outlook.jp>
2023-05-03 13:46:41 +0900

    change the list name

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -7,17 +7,10 @@ def search(list, target):
         return True
     return False
 
-int_list = [1,2,3,4]
-char_list = ['a', 'b', 'c', 'd']
+my_list = [1,2,3,4]
 
-result = access(int_list, 3)
+result = access(my_list, 3)
 print(result)
 
-result = access(char_list, 0)
-print(result)
-
-result = search(int_list, 2)
-print(result)
-
-result = search(char_list, 'x')
+result = search(my_list, 2)
 print(result)
