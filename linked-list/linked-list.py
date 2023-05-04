c02c30a7579be89f95df8c3a386dbd6c09796796
Haruto Mori <baystarsman@outlook.jp>
2023-05-04 13:10:41 +0900

    build a function to check the size of linked list

--- linked-list/linked-list.py
+++ linked-list/linked-list.py
@@ -23,3 +23,17 @@ class LinkedList:
 
     def is_empty(self):
         return self.head == None
+    
+    def size(self):
+        """
+        Returns the number of nodes in the list
+        Takes O(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+        
+        return count
