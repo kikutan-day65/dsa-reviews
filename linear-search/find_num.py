a6daaec761acd152b4837ba537e0840bdfd24004
Haruto Mori <baystarsman@outlook.jp>
2023-05-10 14:52:28 +0900

    find target num and its index with linear search

--- linear-search/find_num.py
+++ linear-search/find_num.py
@@ -6,10 +6,29 @@ def find_num(mylist, target):
             return flag
     return flag
 
+def find_num_index(mylist, target):
+    flag = False
+    found_id = -1
+    for i in range(len(mylist)):
+        if mylist[i] == target:
+            flag = True
+            found_id = i
+            return flag, found_id
+    return flag
+
+
 int_list = [1,2,3,4,5,6,7,8,9,10]
 result = find_num(int_list, 5)
 print(result)
 
 int_list = [1,2,3,4,5,6,7,8,9,10]
 result = find_num(int_list, 11)
+print(result)
+
+int_list = [1,2,3,4,5,6,7,8,9,10]
+result = find_num_index(int_list, 3)
+print(result)
+
+int_list = [1,2,3,4,5,6,7,8,9,10]
+result = find_num_index(int_list, 10)
 print(result)
