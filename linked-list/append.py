0ccf38458c69db5f019421453e7790e7f73aad45
Haruto Mori <baystarsman@outlook.jp>
2023-05-13 10:29:23 +0900

    linked list prepend

--- linked-list/append.py
+++ linked-list/append.py
@@ -7,12 +7,16 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    #=====================================================================
+
     def print_list(self):
         temp = self.head
         while temp:
             print(temp.data, end=' ')
             temp = temp.next
         return True
+
+    #=====================================================================
 
     def append(self, data):
         new_node = Node(data)
@@ -28,6 +32,22 @@ class LinkedList:
         temp.next = new_node
 
         return True
+    
+    #=====================================================================
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return True
+        
+        temp = self.head
+        new_node.next = temp
+        self.head = new_node
+        
+        return True
+
 
 linked_list = LinkedList()
 linked_list.append(1)
@@ -36,3 +56,13 @@ linked_list.append(3)
 linked_list.append(4)
 
 linked_list.print_list()
+
+print()
+
+linked_list2 = LinkedList()
+linked_list2.prepend(10)
+linked_list2.prepend(9)
+linked_list2.prepend(8)
+linked_list2.prepend(7)
+
+linked_list2.print_list()
