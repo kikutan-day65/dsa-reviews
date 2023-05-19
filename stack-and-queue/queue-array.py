cdbd77caac15af9d2bb3522a133fcdf66e8cdb45
Haruto Mori <baystarsman@outlook.jp>
2023-05-19 13:20:53 +0900

    queue dequeue

--- stack-and-queue/queue-array.py
+++ stack-and-queue/queue-array.py
@@ -12,6 +12,11 @@ def enqueue(queue, data):
     queue.insert(0, data)
     return True
 
+
+def dequeue(queue):
+    queue.pop()
+    return True
+
 queue1 = create_queue()
 enqueue(queue1, 0)
 enqueue(queue1, 1)
@@ -19,3 +24,12 @@ enqueue(queue1, 2)
 enqueue(queue1, 3)
 
 print(queue1)
+
+queue2 = create_queue()
+enqueue(queue2, 0)
+enqueue(queue2, 1)
+enqueue(queue2, 2)
+enqueue(queue2, 3)
+dequeue(queue2)
+
+print(queue2)
