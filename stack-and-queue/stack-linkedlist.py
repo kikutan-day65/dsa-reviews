27e32724da6338b7ec97c77bec9d5b4079eaa349
Haruto Mori <baystarsman@outlook.jp>
2023-05-19 11:35:34 +0900

    stack function to check if it's empty

--- stack-and-queue/stack-linkedlist.py
+++ stack-and-queue/stack-linkedlist.py
@@ -6,3 +6,15 @@ class StackNode:
 class Stack:
     def __init__(self):
         self.root = None
+    
+    #================================
+
+    def is_empty(self):
+        if self.root is None:
+            return True
+        return False
+    
+
+
+stack1 = Stack()
+print(stack1.is_empty())
