56a7d6bc136b3b61382cf3f62ae7417a6c681d19
Haruto Mori <baystarsman@outlook.jp>
2023-05-19 13:16:15 +0900

    queue enqueue

--- stack-and-queue/queue-array.py
+++ stack-and-queue/queue-array.py
@@ -2,10 +2,20 @@ def create_queue():
     queue = []
     return queue
 
+
 def is_empty(queue):
     if len(queue) == 0:
         return True
 
 
+def enqueue(queue, data):
+    queue.insert(0, data)
+    return True
 
 queue1 = create_queue()
+enqueue(queue1, 0)
+enqueue(queue1, 1)
+enqueue(queue1, 2)
+enqueue(queue1, 3)
+
+print(queue1)
