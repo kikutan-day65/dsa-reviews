22de30a223aa054e2641c053fbc94ee2fa2e5128
Haruto Mori <baystarsman@outlook.jp>
2023-05-19 13:59:28 +0900

    queue dequeue

--- stack-and-queue/queue-linkedlist.py
+++ stack-and-queue/queue-linkedlist.py
@@ -28,11 +28,32 @@ class Queue:
 
     def enqueue(self, data):
         new_node = QueueNode(data)
+        if self.is_empty():
+            self.front = new_node
+            self.rear = new_node
+            return True
+        
         temp = self.front
         new_node.next = temp
         self.front = new_node
 
         return True
+    
+    #=====================================
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Stack is empty")
+        
+        temp = self.front
+        prev = self.front
+        while temp.next:
+            prev = temp
+            temp = temp.next
+
+        prev.next = None
+        
+        return temp
 
 
 queue1 = Queue()
@@ -42,3 +63,14 @@ queue1.enqueue(2)
 queue1.enqueue(3)
 
 queue1.print_list()
+
+print()
+
+queue2 = Queue()
+queue2.enqueue(0)
+queue2.enqueue(1)
+queue2.enqueue(2)
+queue2.enqueue(3)
+queue2.dequeue()
+
+queue2.print_list()
