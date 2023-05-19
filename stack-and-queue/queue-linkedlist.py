0665478c888caafc2c91df47da528e0ac0aef9ca
Haruto Mori <baystarsman@outlook.jp>
2023-05-19 13:31:57 +0900

    queue function to check if empty and print list

--- stack-and-queue/queue-linkedlist.py
+++ stack-and-queue/queue-linkedlist.py
@@ -8,4 +8,18 @@ class Queue:
     def __init__(self):
         self.front = None
         self.rear = None
+
+    #=====================================
+
+    def print_list(self):
+        temp = self.front
+        while temp:
+            print(temp.data)
+            temp = temp.next
     
+    #=====================================
+
+    def is_empty(self, data):
+        if self.front == None:
+            return True
+        return False
