e9ddbaed63bab80fa810663661a14b2c27daacf3
Haruto Mori <baystarsman@outlook.jp>
2023-05-19 11:44:39 +0900

    stack push item function and print it

--- stack-and-queue/stack-linkedlist.py
+++ stack-and-queue/stack-linkedlist.py
@@ -6,7 +6,7 @@ class StackNode:
 class Stack:
     def __init__(self):
         self.root = None
-    
+
     #================================
 
     def is_empty(self):
@@ -14,7 +14,29 @@ class Stack:
             return True
         return False
     
+    #================================
+
+    def print_stack(self):
+        temp = self.root
+        while temp:
+            print(temp.data, end=' ')
+            temp = temp.next
+        return True
+    
+    #================================
+
+    def push(self, data):
+        new_node = StackNode(data)
+        new_node.next = self.root
+        self.root = new_node
+
+        return True
 
 
 stack1 = Stack()
+stack1.push(0)
+stack1.push(1)
+stack1.push(2)
 print(stack1.is_empty())
+
+stack1.print_stack()
