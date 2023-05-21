5c94aedd2f9764ef66ae82a1cec45920565eb06c
Haruto Mori <baystarsman@outlook.jp>
2023-05-21 11:17:00 +0900

    min-heap has parent, right child, left child

--- heaps/min-heap.py
+++ heaps/min-heap.py
@@ -12,9 +12,10 @@
 class MinHeap:
     def __init__(self, capacity):
         self.storage = [0] * capacity
-        self.capacity + capacity
+        self.capacity = capacity
         self.size = 0
 
+    #======================================================
 
     def get_parent_index(self, index):
         return (index - 1) // 2
@@ -26,3 +27,18 @@ class MinHeap:
 
     def  get_right_child_index(self, index):
         return 2 * index + 2
+    
+    #======================================================
+
+    def has_parent(self, index):
+        return self.get_parent_index(index) >= 0
+    
+
+    def has_left_child(self, index):
+        return self.get_left_child_index(index) < self.size
+    
+    
+    def has_right_child(self, index):
+        return self.get_right_child_index(index) < self.size
+    
+    #======================================================
