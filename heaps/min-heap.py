8c6302f8b95d8f7b49f22f854faa7dd403073c06
Haruto Mori <baystarsman@outlook.jp>
2023-05-21 11:02:36 +0900

    min-heap instruction

---
+++ heaps/min-heap.py
@@ -0,0 +1,11 @@
+"""
+- Evry parent node can have at most only 2 children
+- must be a complete tree
+- Every parent's key must be smaller than its chldren nodes
+
+- representing our heap
+    PARENT INDEX:   (index - 1) // 2
+    LEFT CHILD INDEX:   2 * index + 1
+    RIGHT CHILD INDEX:  2 * index + 2
+"""
+
